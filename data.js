d8f51afed585c66d271df36738b73cc820f2d8a2
Mustavojj <mostavojj@gmail.com>
2026-06-05 17:58:28 +0300

    Add APP_CONFIG with application settings

---
+++ data.js
@@ -0,0 +1,27 @@
+export const APP_CONFIG = {
+    APP_NAME: "ZENTRIX",
+    BOT_USERNAME: "Zentrxbot",
+    SUPPORT_LINK: "https://t.me/Zentrxb",
+    MINIMUM_WITHDRAW: 0.01,
+    REFERRAL_PERCENTAGE: 20,
+    MINING_SESSION_HOURS: 8,
+    POWER_PER_TON_RATE: 0.0000833333,
+    TASK_VERIFICATION_DELAY: 10,
+    DEFAULT_USER_AVATAR: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg",
+    AD_COOLDOWN_HOURS: 6,
+    REWARD_AD_BLOCK_ID: "32205",
+    INTERSTITIAL_AD_BLOCK_ID: "int-32206",
+    BOT_LINK: "https://t.me/Zentrxbot/mine?startapp=",
+    DAILY_CHECK_NEWS_LINK: "https://t.me/Zentrxb",
+    STAR_PRICE_PER_100: 50,
+    REFERRAL_REQUIRED_TASKS: 5,
+    REFERRAL_REQUIRED_MINES: 2,
+    BOT_TOKEN: "",
+    MAIN_TASKS: [
+        { id: "money_hub", name: "MONEY HUB", url: "https://t.me/MONEYHUB9_69", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" },
+        { id: "ton_labs", name: "TON LABS", url: "https://t.me/TonLabstg", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" },
+        { id: "gorilla", name: "GORILLA", url: "https://t.me/ZOBRABZA", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" },
+        { id: "get_ref", name: "GET REFERRALS", url: "https://t.me/NEJARS", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" },
+        { id: "ref_all", name: "REFERRALS FOR ALL", url: "https://t.me/getrefss190", reward: 10, verify: false, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" }
+    ]
+};
