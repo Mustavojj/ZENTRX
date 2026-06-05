3bd5cd18658ca72023638b37578b8e40a90810e4
Mustavojj <mostavojj@gmail.com>
2026-06-05 23:52:20 +0300

    Implement ad display in handleClaim function
    Added error handling for ad display in handleClaim function.

--- js/app.js
+++ js/app.js
@@ -423,6 +423,11 @@ class App {
         const closeBtn = document.getElementById('close-claim-modal');
         
         const handleClaim = async () => {
+            try {
+                const AdController = window.Adsgram.init({ blockId: APP_CONFIG.INTERSTITIAL_AD_BLOCK_ID });
+                await AdController.show();
+            }
+            
             const earnedAmount = this.pendingTonReward;
             const originalTon = this.tonBalance;
             this.tonBalance += earnedAmount;
