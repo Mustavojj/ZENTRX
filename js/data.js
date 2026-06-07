e14a422e49c500acfdf6cf05561370b374dcf3c2
Mustavojj <mostavojj@gmail.com>
2026-06-07 17:21:13 +0300

    Update APP_CONFIG with new values and links

--- js/data.js
+++ js/data.js
@@ -1,16 +1,15 @@
 export const APP_CONFIG = {
     APP_NAME: "ZENTRIX",
     BOT_USERNAME: "Zentrxbot",
-    SUPPORT_LINK: "",
     MINIMUM_WITHDRAW: 0.01,
-    WITHDRAWAL_FEES: 0.01,
+    WITHDRAWAL_FEES: 0.0005,
     REFERRAL_PERCENTAGE: 20,
-    REFERRAL_POWER_REWARD: 500,
+    REFERRAL_POWER_REWARD: 300,
     MINING_SESSION_HOURS: 5,
     POWER_PER_TON_RATE: 0.00002,
     POWER_PER_DAY_RATE: 0.0001,
     TASK_VERIFICATION_DELAY: 10,
-    DEFAULT_USER_AVATAR: "https://i.ibb.co/JwHWrrX3/4ef418a7e09b.jpg",
+    DEFAULT_USER_AVATAR: "https://i.ibb.co/bjyVgYqJ/256e636cf3a0.jpg",
     AD_COOLDOWN_HOURS: 6,
     REWARD_AD_BLOCK_ID: "32205",
     INTERSTITIAL_AD_BLOCK_ID: "int-32206",
@@ -19,15 +18,16 @@ export const APP_CONFIG = {
     STAR_PRICE_PER_100: 25,
     REFERRAL_REQUIRED_TASKS: 5,
     REFERRAL_REQUIRED_MINES: 2,
-    BOT_TOKEN: "",
+    TASK_REWARD: 10,
+    TASK_IMAGE: "https://i.ibb.co/bjyVgYqJ/256e636cf3a0.jpg",
     DAILY_TASKS: [
         { id: "daily_check_news", name: "Daily Check News", url: "https://t.me/Zentrxb", reward: 10, verify: true }
     ],
     MAIN_TASKS: [
-        { id: "money_hub", name: "MONEY HUB", url: "https://t.me/MONEYHUB9_69", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" },
-        { id: "ton_labs", name: "TON LABS", url: "https://t.me/TonLabstg", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" },
-        { id: "gorilla", name: "GORILLA", url: "https://t.me/ZOBRABZA", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" },
-        { id: "get_ref", name: "GET REFERRALS", url: "https://t.me/NEJARS", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" },
-        { id: "ref_all", name: "REFERRALS FOR ALL", url: "https://t.me/getrefss190", reward: 10, verify: false, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" }
+        { id: "money_hub", name: "MONEY HUB", url: "https://t.me/MONEYHUB9_69", reward: 10, verify: true },
+        { id: "ton_labs", name: "TON LABS", url: "https://t.me/TonLabstg", reward: 10, verify: true },
+        { id: "gorilla", name: "GORILLA", url: "https://t.me/ZOBRABZA", reward: 10, verify: true },
+        { id: "get_ref", name: "GET REFERRALS", url: "https://t.me/NEJARS", reward: 10, verify: true },
+        { id: "ref_all", name: "REFERRALS FOR ALL", url: "https://t.me/getrefss190", reward: 10, verify: false }
     ]
 };
