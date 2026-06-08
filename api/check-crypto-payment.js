73c67597e47be1a54952831a13b8f6fa39f573c4
Mustavojj <mostavojj@gmail.com>
2026-06-08 22:18:46 +0300

    Fix import path for APP_CONFIG in check-crypto-payment.js

--- api/check-crypto-payment.js
+++ api/check-crypto-payment.js
@@ -1,4 +1,4 @@
-import { APP_CONFIG } from '../js/data.js';
+import { APP_CONFIG } from './js/data.js';
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
