9f16f6f282f0461467cde7161787af15e9e80ab9
Mustavojj <mostavojj@gmail.com>
2026-06-16 15:31:22 +0300

    Update referral power reward and quest rewards

--- js/data.js
+++ js/data.js
@@ -4,7 +4,7 @@ export const APP_CONFIG = {
     MINIMUM_WITHDRAW: 0.01,
     WITHDRAWAL_FEES: 0,
     REFERRAL_PERCENTAGE: 10,
-    REFERRAL_POWER_REWARD: 3000,
+    REFERRAL_POWER_REWARD: 5000,
     MINING_SESSION_HOURS: 5,
     POWER_PER_TON_RATE: 0.00000416667,
     POWER_PER_DAY_RATE: 0.0001,
@@ -26,7 +26,7 @@ export const APP_CONFIG = {
     TASK_IMAGE: "https://i.ibb.co/bjyVgYqJ/256e636cf3a0.jpg",
     DAILY_TASKS: [
         { id: "daily_check_news", name: "Daily Check News", url: "https://t.me/Zentrxb", reward: 100, verify: true, icon: "fa-newspaper" },
-    ],
+        ],
     MAIN_TASKS: [
         { id: "money_hub", name: "MONEY HUB", url: "https://t.me/MONEYHUB9_69", reward: 100, verify: true },
         { id: "ton_labs", name: "TON LABS", url: "https://t.me/TonLabstg", reward: 100, verify: true },
@@ -41,23 +41,23 @@ export const APP_CONFIG = {
             { target_level: 2, reward: 1000 },
             { target_level: 3, reward: 2000 },
             { target_level: 4, reward: 3000 },
-            { target_level: 5, reward: 4000 },
-            { target_level: 6, reward: 5000 },
-            { target_level: 7, reward: 6000 },
-            { target_level: 8, reward: 7000 },
-            { target_level: 9, reward: 8000 },
-            { target_level: 10, reward: 10000 }
+            { target_level: 5, reward: 3000 },
+            { target_level: 6, reward: 3000 },
+            { target_level: 7, reward: 3000 },
+            { target_level: 8, reward: 3000 },
+            { target_level: 9, reward: 3000 },
+            { target_level: 10, reward: 3000 }
         ],
         mining_quests: [
             { target_starts: 3, reward: 1000 },
             { target_starts: 5, reward: 2000 },
             { target_starts: 10, reward: 3000 },
-            { target_starts: 20, reward: 4000 },
-            { target_starts: 30, reward: 5000 },
-            { target_starts: 40, reward: 6000 },
-            { target_starts: 50, reward: 7000 },
-            { target_starts: 75, reward: 8000 },
-            { target_starts: 100, reward: 10000 }
+            { target_starts: 20, reward: 3000 },
+            { target_starts: 30, reward: 3000 },
+            { target_starts: 40, reward: 3000 },
+            { target_starts: 50, reward: 3000 },
+            { target_starts: 75, reward: 3000 },
+            { target_starts: 100, reward: 3000 }
         ]
     }
 };
